79fb891d0440e41ec163f07ec1aae3f2b5e7100a
Shacaa <elshaca95@gmail.com>
2022-11-13 20:38:09 -0300

    adds speech recognition

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      HOLA
-    </div>
-  );
+    const {
+        transcript,
+        listening,
+        resetTranscript
+    } = useSpeechRecognition({clearTranscriptOnListen: false});
+    const [lastTranscript, setLastTranscript] = useState('');
+
+    const startListening = () => {
+        SpeechRecognition.startListening({language: 'es-AR'})
+            .catch(err => console.error(err.message));
+    };
+
+    useEffect(startListening, []);
+
+    const setCleanTimer = () => {
+        setTimeout(() => setLastTranscript(''), 5000);
+    };
+
+    useEffect(() => {
+        if (!listening) {
+            if (transcript !== '') {
+                setLastTranscript(transcript);
+                setCleanTimer();
+            }
+            resetTranscript();
+            startListening();
+        }
+    }, [listening]);
+
+    return (
+        <div>
+            {lastTranscript}
+        </div>
+    );
 }
 
 export default App;
