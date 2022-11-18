64cb9b4252b5b41a516d9642c8fb4e3494b04f68
Shacaa <elshaca95@gmail.com>
2022-11-18 00:05:33 -0300

    adds translate api call on front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition';
+import axios from 'axios';
 import './App.css';
+
+const LANG_SRC = 'es-AR';
+const LANG_TARGET = 'en';
+const BACK_URL = 'http://localhost:8080/translate';
 
 function App() {
     const {
@@ -9,22 +14,33 @@ function App() {
         resetTranscript
     } = useSpeechRecognition({clearTranscriptOnListen: false});
     const [lastTranscript, setLastTranscript] = useState('');
+    const [lastTranslation, setLastTranslation] = useState('');
 
     const startListening = () => {
-        SpeechRecognition.startListening({language: 'es-AR'})
+        SpeechRecognition.startListening({language: LANG_SRC})
             .catch(err => console.error(err.message));
     };
 
     useEffect(startListening, []);
 
     const setCleanTimer = () => {
-        setTimeout(() => setLastTranscript(''), 5000);
+        setTimeout(() => {
+            setLastTranscript('');
+            setLastTranslation('');
+        }, 5000);
     };
+
+    const sendToTranslate = (value: string) => {
+        axios.get(`${BACK_URL}?source=${LANG_SRC}&target=${LANG_TARGET}&text=${value}`)
+            .then(response => setLastTranslation(response.data))
+            .catch(err => console.error(err.message));
+    }
 
     useEffect(() => {
         if (!listening) {
             if (transcript !== '') {
                 setLastTranscript(transcript);
+                sendToTranslate(transcript);
                 setCleanTimer();
             }
             resetTranscript();
@@ -35,6 +51,7 @@ function App() {
     return (
         <div>
             {lastTranscript}
+            {lastTranslation}
         </div>
     );
 }
