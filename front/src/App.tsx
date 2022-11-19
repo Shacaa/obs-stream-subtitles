39dcbf0cbb8450954ced27f3a3e95282f02281fd
Shacaa <elshaca95@gmail.com>
2022-11-19 00:54:47 -0300

    adds buttons to start/stop listening

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -15,13 +15,18 @@ function App() {
     } = useSpeechRecognition({clearTranscriptOnListen: false});
     const [lastTranscript, setLastTranscript] = useState('');
     const [lastTranslation, setLastTranslation] = useState('');
+    const [shouldListen, setShouldListen] = useState(false);
 
     const startListening = () => {
         SpeechRecognition.startListening({language: LANG_SRC})
             .catch(err => console.error(err.message));
     };
 
-    useEffect(startListening, []);
+    const stopListening = () => {
+        SpeechRecognition.stopListening();
+    };
+
+    useEffect(stopListening, []);
 
     const setCleanTimer = () => {
         setTimeout(() => {
@@ -37,7 +42,7 @@ function App() {
     }
 
     useEffect(() => {
-        if (!listening) {
+        if (!listening && shouldListen) {
             if (transcript !== '') {
                 setLastTranscript(transcript);
                 sendToTranslate(transcript);
@@ -48,10 +53,25 @@ function App() {
         }
     }, [listening]);
 
+    const onClick = () => {
+        setShouldListen(!listening);
+        if (listening) {
+            stopListening();
+        } else {
+            startListening()
+        }
+    };
+
     return (
-        <div>
-            {lastTranscript}
-            {lastTranslation}
+        <div className={'app-container'}>
+            <button
+                className={`app-button ${shouldListen ? 'app-button-red' : 'app-button-green'}`}
+                onClick={onClick}
+            >
+                {shouldListen ? 'Stop listening' : 'Start listening'}
+            </button>
+            <p className={'app-transcript'}>{lastTranscript}</p>
+            <p className={'app-translation'}>{lastTranslation}</p>
         </div>
     );
 }
