0aa166955e0530b49fec45bcf4baf40f4623f346
Shacaa <33043789+Shacaa@users.noreply.github.com>
2023-04-07 00:22:37 -0300

    Feat: Adds switch language button and fixes clearTimeout (#2)
    - Adds switch language button (from ES to EN)
    - Fixes timeout of clearing of transcript and translation

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -3,8 +3,8 @@ import SpeechRecognition, {useSpeechRecognition} from 'react-speech-recognition'
 import axios from 'axios';
 import './App.css';
 
-const LANG_SRC = 'es-AR';
-const LANG_TARGET = 'en';
+const LANG_ES = 'es-AR';
+const LANG_EN = 'en';
 const BACK_URL = 'http://localhost:8080/translate';
 
 function App() {
@@ -15,10 +15,15 @@ function App() {
     } = useSpeechRecognition({clearTranscriptOnListen: false});
     const [lastTranscript, setLastTranscript] = useState('');
     const [lastTranslation, setLastTranslation] = useState('');
+    const [clearTranscriptTimeoutID, setClearTranscriptTimeoutID] = useState(0);
     const [shouldListen, setShouldListen] = useState(false);
+    const [srcLang, setSrcLang] = useState(LANG_ES);
+    const [targetLang, setTargetLang] = useState(LANG_EN);
 
     const startListening = () => {
-        SpeechRecognition.startListening({language: LANG_SRC})
+        console.log('start listening src', srcLang);
+        console.log('start listening target', targetLang);
+        SpeechRecognition.startListening({language: srcLang})
             .catch(err => console.error(err.message));
     };
 
@@ -28,15 +33,18 @@ function App() {
 
     useEffect(stopListening, []);
 
-    const setCleanTimer = () => {
-        setTimeout(() => {
+    const setClearTimer = () => {
+        clearTimeout(clearTranscriptTimeoutID);
+        const timeoutID = setTimeout(() => {
             setLastTranscript('');
             setLastTranslation('');
         }, 5000);
+        // @ts-ignore
+        setClearTranscriptTimeoutID(timeoutID);
     };
 
     const sendToTranslate = (value: string) => {
-        axios.get(`${BACK_URL}?source=${LANG_SRC}&target=${LANG_TARGET}&text=${value}`)
+        axios.get(`${BACK_URL}?source=${srcLang}&target=${targetLang}&text=${value}`)
             .then(response => setLastTranslation(response.data))
             .catch(err => console.error(err.message));
     }
@@ -46,14 +54,14 @@ function App() {
             if (transcript !== '') {
                 setLastTranscript(transcript);
                 sendToTranslate(transcript);
-                setCleanTimer();
+                setClearTimer();
             }
             resetTranscript();
             startListening();
         }
     }, [listening]);
 
-    const onClick = () => {
+    const onSwitchRecording = () => {
         setShouldListen(!listening);
         if (listening) {
             stopListening();
@@ -62,15 +70,35 @@ function App() {
         }
     };
 
+    const onSwitchLanguage = () => {
+        const newSrc = srcLang === LANG_ES ? LANG_EN : LANG_ES;
+        const newTarget = targetLang === LANG_ES ? LANG_EN : LANG_ES;
+        setSrcLang(newSrc);
+        setTargetLang(newTarget);
+        startListening();
+        setShouldListen(true);
+    };
+
     return (
         <div className={'app-container'}>
             <div className={'app-container-main'}>
-                <button
-                    className={`app-button ${shouldListen ? 'app-button-red' : 'app-button-green'}`}
-                    onClick={onClick}
-                >
-                    {shouldListen ? 'Stop listening' : 'Start listening'}
-                </button>
+                <h1>
+                    {`${srcLang} ---> ${targetLang}`}
+                </h1>
+                <div className={'app-buttons-container'}>
+                    <button
+                        className={`app-button ${shouldListen ? 'app-button-red' : 'app-button-green'}`}
+                        onClick={onSwitchRecording}
+                    >
+                        {shouldListen ? 'Stop listening' : 'Start listening'}
+                    </button>
+                    <button
+                        className={'app-button'}
+                        onClick={onSwitchLanguage}
+                    >
+                        Switch language
+                    </button>
+                </div>
                 <p className={'app-transcript'}>{lastTranscript}</p>
                 <p className={'app-translation'}>{lastTranslation}</p>
             </div>
